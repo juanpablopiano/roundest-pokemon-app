9ee491e8ad6812106fe72f984546ba0a765dc4c3
Juan Gonzalez <jgu1@hotmail.com>
2022-01-15 16:02:32 -0300

    Add tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
-const Home = () => {
-  return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text center">Which Pokémon is rounder?</div>
-      <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200" />
-        <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
-      </div>
-    </div>
-  )
-}
+import { trpc } from "@/utils/trpc";
 
-export default Home
+const Home = () => {
+	return (
+		<div className='h-screen w-screen flex flex-col justify-center items-center'>
+			<div className='text-2xl text center'>
+				Which Pokémon is rounder?
+			</div>
+			<div className='p-2' />
+			<div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+				<div className='w-16 h-16 bg-red-200' />
+				<div className='p-8'>Vs</div>
+				<div className='w-16 h-16 bg-red-200' />
+			</div>
+		</div>
+	);
+};
+
+export default Home;
