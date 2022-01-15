0f1e9d3f7262319e803c0b89df092ad691410320
Juan Gonzalez <jgu1@hotmail.com>
2022-01-15 14:50:35 -0300

    Upgrade tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-	mode: 'jit',
-	purge: [
+	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
-	darkMode: false,
-	content: [],
+	darkMode: 'media',
 	theme: {
 		extend: {},
 	},
