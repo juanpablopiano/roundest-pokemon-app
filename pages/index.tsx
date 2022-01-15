5e063e52daf1e06b3983c790e0a5390705302266
Juan Gonzalez <jgu1@hotmail.com>
2022-01-15 14:56:04 -0300

    Create basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 const Home = () => {
   return (
-    <div className="">Hello world</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text center">Which Pokémon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
   )
 }
 
