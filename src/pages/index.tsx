e9b5aa64f23d38f344bf72bca01c4f5154baae94
Juan Gonzalez <jgu1@hotmail.com>
2022-01-16 18:37:25 -0300

    Modify data sent by backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
+// import Image from 'next/image';
+import { useState } from 'react';
 
 const Home = () => {
+	const [ids, updateIds] = useState(() => getOptionsForVote());
 
-	const [first, second] = getOptionsForVote();
+	const [first, second] = ids;
+
+	const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }]);
+	const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }]);
+
+	if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
 	return (
 		<div className='h-screen w-screen flex flex-col justify-center items-center'>
@@ -12,9 +20,24 @@ const Home = () => {
 			</div>
 			<div className='p-2' />
 			<div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-				<div className='pkm-container'>{first}</div>
+				<div className='w-64 h-64 flex flex-col'>
+					<img
+						src={firstPokemon.data?.sprites.front_default}
+						alt={firstPokemon.data?.name}
+						className='w-full'
+					/>
+          <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+				</div>
 				<div className='p-8'>Vs</div>
-				<div className='pkm-container'>{second}</div>
+				<div className='w-64 h-64 flex flex-col'>
+					<img
+						src={secondPokemon.data?.sprites.front_default}
+						alt={secondPokemon.data?.name}
+						className='w-full'
+					/>
+          <div className="text-xl text-center capitalize mt-[-2rem] ">{secondPokemon.data?.name}</div>
+				</div>
+        <div className="p-2" />
 			</div>
 		</div>
 	);
